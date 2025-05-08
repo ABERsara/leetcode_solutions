14585ac0619f811f0e27d1d4cf68deddafc3d067
Sara Aber <155323880+ABERsara@users.noreply.github.com>
2025-05-08 13:18:48 +0300

    File updated at 5/8/2025, 1:18:47 PM

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.py
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 5/8/2025, 1:13:53 PM
+# Last updated: 5/8/2025, 1:18:47 PM
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -11,13 +11,20 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
+        # check if headA and headB exist 
+        # nested while loop to traverse the linked list
+        # for each node in list a check if it equals a node in list b
+        # if it does then return that node 
+        # otherwise keep traversing
+        # if it gets to the end then return none
         if not headA or not headB:
             return None
-    
-        pointerA, pointerB = headA, headB
-    
-        while pointerA != pointerB:
-            pointerA = pointerA.next if pointerA else headB
-            pointerB = pointerB.next if pointerB else headA
-        return pointerA
-    
+
+        pointer1, pointer2 = headA, headB
+
+        while pointer1 != pointer2:
+            pointer1 = pointer1.next if pointer1 else headB
+            pointer2 = pointer2.next if pointer2 else headA
+
+        return pointer1
+        

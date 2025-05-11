c68b8c7dc436a3e46de70a3f41fe620659059628
Sara Aber <155323880+ABERsara@users.noreply.github.com>
2025-05-11 16:15:50 +0300

    Create file

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,16 @@
+# Last updated: 5/11/2025, 4:15:51 PM
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
+
+
+        
